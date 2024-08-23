1038fbc8b3e7d6a1e29a4944529cee57282cd9c6
Adi00000076 <adinarayana.k@infyzterminals.com>
2024-08-23 11:40:54 +0530

    first commit

--- main.js
+++ main.js
@@ -29,3 +29,6 @@ function createWindow () {
 }
 
 app.whenReady().then(createWindow);
+
+
+
--- server.js
+++ server.js
@@ -1,6 +1,37 @@
+// const express = require('express');
+// const cors = require('cors');
+// const { exec } = require('child_process');
+// const app = express();
+// const port = 3000;
+
+// // Use the cors middleware
+// app.use(cors());
+
+// app.get('/run-exe', (req, res) => {
+//   exec('C:\\Users\\infyz\\Desktop\\app.exe', (error, stdout, stderr) => {
+//     if (error) {
+//       res.status(500).send(`Error: ${error.message}`);
+//       return;
+//     }
+//     if (stderr) {
+//       res.status(500).send(`stderr: ${stderr}`);
+//       return;
+//     }
+//     res.send(`stdout: ${stdout}`);
+//   });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}/`);
+// });
+
+
+
+
 const express = require('express');
 const cors = require('cors');
 const { exec } = require('child_process');
+const path = require('path');
 const app = express();
 const port = 3000;
 
@@ -8,7 +39,10 @@ const port = 3000;
 app.use(cors());
 
 app.get('/run-exe', (req, res) => {
-  exec('C:\\Users\\infyz\\Desktop\\app.exe', (error, stdout, stderr) => {
+  // Construct the path to the .exe file in the current directory
+  const exePath = path.join(__dirname, 'app.exe');
+
+  exec(exePath, (error, stdout, stderr) => {
     if (error) {
       res.status(500).send(`Error: ${error.message}`);
       return;
